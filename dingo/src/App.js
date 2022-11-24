b9831c5f280e6616384accbaed6901d65eca703d
Fowl3r <tcd.fowler@gmail.com>
2022-11-24 16:17:37 +0000

    working on the flexbox & css

--- dingo/src/App.js
+++ dingo/src/App.js
@@ -6,7 +6,42 @@ import InputForm from "./components/InputForm/InputForm";
 function App() {
 	// create state for cards
 	const [cards, setCards] = useState([
-
+		{
+			author: "Coach",
+			category: "React",
+			difficulty: 1,
+			estimated_time: "< 30 mins",
+			format: "Video",
+			link: "https://www.youtube.com/watch?v=-yrnWnN0g9o",
+			title: "React With Firebase",
+		},
+		{
+			author: "Coach",
+			category: "Node.js",
+			difficulty: 3,
+			estimated_time: "< 30 mins",
+			format: "Video",
+			link: "https://www.youtube.com/watch?v=-yrnWnN0g9o",
+			title: "Node.js begginer guide",
+		},
+		{
+			author: "Coach",
+			category: "UI/UX",
+			difficulty: 1,
+			estimated_time: "< 30 mins",
+			format: "Video",
+			link: "https://www.youtube.com/watch?v=-yrnWnN0g9o",
+			title: "Principles of design",
+		},
+		{
+			author: "Coach",
+			category: "API",
+			difficulty: 2,
+			estimated_time: "< 30 mins",
+			format: "Video",
+			link: "https://www.youtube.com/watch?v=-yrnWnN0g9o",
+			title: "How programmes communicate",
+		},
 	]);
 
 	/* 
--- dingo/src/components/CardItem/CardItem.js
+++ dingo/src/components/CardItem/CardItem.js
@@ -1,30 +1,77 @@
 import React from "react";
 import "./CardItem.css";
-import dingo from "./dingo.png"
+import dingo from "./dingo.png";
 
-function CardItem({ image, title, difficulty, category, link, author }) {
+function CardItem({ title, difficulty, category, link, author }) {
 	return (
 		<div className="cardItem">
-			
 			{/* here is where we're conditionally rendering the different topic cards using external URLs*/}
-			<div className = "topic-image">
-				{category === "React" ? <img src="https://cdn-icons-png.flaticon.com/512/1183/1183672.png
-				" alt="react logo"/> : null}
-				{category === "API" ? <img src="https://cdn-icons-png.flaticon.com/512/2172/2172831.png" alt="API logo"/> : null}
-				{category === "Basic JavaScript" ? <img src="https://cdn-icons-png.flaticon.com/512/1199/1199124.png" alt="Basic JavaScript logo"/> : null}
-				{category === "Advanced JavaScript" ? <img src="https://e7.pngegg.com/pngimages/130/641/png-clipart-javascript-node-js-computer-icons-logo-application-software-javascript-icon-text-logo.png" alt="Advanced JavaScript logo"/> : null}
-				{category === "CSS" ? <img src="https://cdn-icons-png.flaticon.com/512/919/919826.png" alt="CSS logo"/> : null}
-				{category === "Miscellaneous" ? <img src="https://cdn-icons-png.flaticon.com/512/1010/1010721.png" alt="Miscellaneous logo"/> : null}
-				{category === "Node.js" ? <img src="https://cdn.iconscout.com/icon/free/png-256/nodejs-2-226035.png" alt="Node.js logo"/> : null}
-				{category === "SQL" ? <img src="https://cdn-icons-png.flaticon.com/128/4248/4248340.png" alt="SQL logo"/> : null}
-				{category === "UI/UX" ? <img src="https://cdn-icons-png.flaticon.com/512/7858/7858975.png" alt="UI/UX logo"/> : null}
-				{category === "Testing" ? <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSffNFCMkCpiaFUO9zQlwIfm5HuobQSdlelQw&usqp=CAU" alt="Testing logo"/> : null}
-
-				
+			<div className="topic-image">
+				{category === "React" ? (
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/1183/1183672.png
+				"
+						alt="react logo"
+					/>
+				) : null}
+				{category === "API" ? (
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/2172/2172831.png"
+						alt="API logo"
+					/>
+				) : null}
+				{category === "Basic JavaScript" ? (
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/1199/1199124.png"
+						alt="Basic JavaScript logo"
+					/>
+				) : null}
+				{category === "Advanced JavaScript" ? (
+					<img
+						src="https://e7.pngegg.com/pngimages/130/641/png-clipart-javascript-node-js-computer-icons-logo-application-software-javascript-icon-text-logo.png"
+						alt="Advanced JavaScript logo"
+					/>
+				) : null}
+				{category === "CSS" ? (
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/919/919826.png"
+						alt="CSS logo"
+					/>
+				) : null}
+				{category === "Miscellaneous" ? (
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/1010/1010721.png"
+						alt="Miscellaneous logo"
+					/>
+				) : null}
+				{category === "Node.js" ? (
+					<img
+						src="https://cdn.iconscout.com/icon/free/png-256/nodejs-2-226035.png"
+						alt="Node.js logo"
+					/>
+				) : null}
+				{category === "SQL" ? (
+					<img
+						src="https://cdn-icons-png.flaticon.com/128/4248/4248340.png"
+						alt="SQL logo"
+					/>
+				) : null}
+				{category === "UI/UX" ? (
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/7858/7858975.png"
+						alt="UI/UX logo"
+					/>
+				) : null}
+				{category === "Testing" ? (
+					<img
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSffNFCMkCpiaFUO9zQlwIfm5HuobQSdlelQw&usqp=CAU"
+						alt="Testing logo"
+					/>
+				) : null}
 			</div>
 
-			<a href={link}>
-				<h1>{title}</h1>
+			<a className="link" href={link}>
+				<h1 className="title">{title}</h1>
 			</a>
 			{difficulty === 1 ? <img src={dingo} alt="dingo" /> : null}
 
@@ -41,9 +88,9 @@ function CardItem({ image, title, difficulty, category, link, author }) {
 					<img src={dingo} alt="dingo" />
 				</div>
 			) : null}
-			
+
 			{/* <p> {difficulty}</p> */}
-			
+
 			<p>{author}</p>
 		</div>
 	);
