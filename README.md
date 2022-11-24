30018e553758f44ac6117fac44871d61366061cc
BushraFatimaBF <vglampard@protonmail.com>
2022-11-24 14:27:12 +0000

    conditional rendering skeleton for topic images sketched out

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # bc13_w9_project-frontend-dingo
 
 readme version 2.0
+
+CSS THINGS TO DO:
+
+    CONTENT
+    - Find and download images (delete background!) in PNG format
+    - conditional rendering in the card component so that different mages are produced according to the category
+   - 
+
+   SPACING AND POSITIONING
+    - Research flexbox so we can apply it to A) spacing within the card item and B) distribution of form and card components
+
+    COLOURS
+    - Implement screen 
+
+    TEXT
--- dingo/src/components/CardItem/CardItem.js
+++ dingo/src/components/CardItem/CardItem.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./CardItem.css";
 
+
 function CardItem({ image, title, difficulty, category, link, author }) {
 	return (
 		<div className="cardItem">
+			
+			{/* here is where we're conditionally rendering the different topic cards using external URLs*/}
+			<div className = "topic-image">
+				{category === "React" ? <img src="https://cdn-icons-png.flaticon.com/512/1183/1183672.png
+				" alt="react logo"/> : null}
+				{category === "API " ? <img src="" alt="API logo"/> : null}
+				{category === "Basic JavaScript" ? <img src="" alt="Basic JavaScript logo"/> : null}
+				{category === "CSS" ? <img src="" alt="CSS logo"/> : null}
+				{category === "Miscellaneous" ? <img src="" alt="Miscellaneous logo"/> : null}
+				{category === "Node.js" ? <img src="" alt="Node.js logo"/> : null}
+				{category === "SQL" ? <img src="" alt="SQL logo"/> : null}
+				{category === "UI/UX" ? <img src="" alt="UI/UX logo"/> : null}
+			</div>
+
+			<a href={link}>
+				<h1>{title}</h1>
+			</a>
 			{difficulty === 1 ? <img src={image} alt="dingo" /> : null}
+
 			{difficulty === 2 ? (
 				<div className="imageContainer">
 					<img src={image} alt="dingo" />
@@ -18,11 +37,9 @@ function CardItem({ image, title, difficulty, category, link, author }) {
 					<img src={image} alt="dingo" />
 				</div>
 			) : null}
-			<a href={link}>
-				<h1>{title}</h1>
-			</a>
+			
 			{/* <p> {difficulty}</p> */}
-			<p>{category}</p>
+			
 			<p>{author}</p>
 		</div>
 	);
