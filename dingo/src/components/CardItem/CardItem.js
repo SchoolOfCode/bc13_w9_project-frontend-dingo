517ade92d2b1759162986b83583e3ed4414216dd
BushraFatimaBF <vglampard@protonmail.com>
2022-11-24 15:27:17 +0000

    testing logo is there now

--- dingo/src/components/CardItem/CardItem.js
+++ dingo/src/components/CardItem/CardItem.js
@@ -18,6 +18,9 @@ function CardItem({ image, title, difficulty, category, link, author }) {
 				{category === "Node.js" ? <img src="https://cdn.iconscout.com/icon/free/png-256/nodejs-2-226035.png" alt="Node.js logo"/> : null}
 				{category === "SQL" ? <img src="https://cdn-icons-png.flaticon.com/128/4248/4248340.png" alt="SQL logo"/> : null}
 				{category === "UI/UX" ? <img src="https://cdn-icons-png.flaticon.com/512/7858/7858975.png" alt="UI/UX logo"/> : null}
+				{category === "Testing" ? <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSffNFCMkCpiaFUO9zQlwIfm5HuobQSdlelQw&usqp=CAU" alt="Testing logo"/> : null}
+
+				
 			</div>
 
 			<a href={link}>
