2c708a3f157cd677231db1be062d40f284cc688e
BushraFatimaBF <vglampard@protonmail.com>
2022-11-24 15:23:30 +0000

    dingo images correctly added

--- dingo/src/components/CardItem/CardItem.js
+++ dingo/src/components/CardItem/CardItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CardItem.css";
-
+import dingo from "./dingo.png"
 
 function CardItem({ image, title, difficulty, category, link, author }) {
 	return (
@@ -13,29 +13,29 @@ function CardItem({ image, title, difficulty, category, link, author }) {
 				{category === "API" ? <img src="https://cdn-icons-png.flaticon.com/512/2172/2172831.png" alt="API logo"/> : null}
 				{category === "Basic JavaScript" ? <img src="https://cdn-icons-png.flaticon.com/512/1199/1199124.png" alt="Basic JavaScript logo"/> : null}
 				{category === "Advanced JavaScript" ? <img src="https://e7.pngegg.com/pngimages/130/641/png-clipart-javascript-node-js-computer-icons-logo-application-software-javascript-icon-text-logo.png" alt="Advanced JavaScript logo"/> : null}
-				{category === "CSS" ? <img src="" alt="CSS logo"/> : null}
-				{category === "Miscellaneous" ? <img src="" alt="Miscellaneous logo"/> : null}
+				{category === "CSS" ? <img src="https://cdn-icons-png.flaticon.com/512/919/919826.png" alt="CSS logo"/> : null}
+				{category === "Miscellaneous" ? <img src="https://cdn-icons-png.flaticon.com/512/1010/1010721.png" alt="Miscellaneous logo"/> : null}
 				{category === "Node.js" ? <img src="https://cdn.iconscout.com/icon/free/png-256/nodejs-2-226035.png" alt="Node.js logo"/> : null}
-				{category === "SQL" ? <img src="" alt="SQL logo"/> : null}
+				{category === "SQL" ? <img src="https://cdn-icons-png.flaticon.com/128/4248/4248340.png" alt="SQL logo"/> : null}
 				{category === "UI/UX" ? <img src="https://cdn-icons-png.flaticon.com/512/7858/7858975.png" alt="UI/UX logo"/> : null}
 			</div>
 
 			<a href={link}>
 				<h1>{title}</h1>
 			</a>
-			{difficulty === 1 ? <img src={image} alt="dingo" /> : null}
+			{difficulty === 1 ? <img src={dingo} alt="dingo" /> : null}
 
 			{difficulty === 2 ? (
 				<div className="imageContainer">
-					<img src={image} alt="dingo" />
-					<img src={image} alt="dingo" />
+					<img src={dingo} alt="dingo" />
+					<img src={dingo} alt="dingo" />
 				</div>
 			) : null}
 			{difficulty === 3 ? (
 				<div className="imageContainer">
-					<img src={image} alt="dingo" />
-					<img src={image} alt="dingo" />
-					<img src={image} alt="dingo" />
+					<img src={dingo} alt="dingo" />
+					<img src={dingo} alt="dingo" />
+					<img src={dingo} alt="dingo" />
 				</div>
 			) : null}
 			
